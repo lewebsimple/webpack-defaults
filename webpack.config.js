943f6ed488d8dfce4a574dff22e364f848bf4e69
Pascal Martineau <pascal@lewebsimple.ca>
2020-10-08 13:21:51 -0400

    Change JS rule a bit

--- webpack.config.js
+++ webpack.config.js
@@ -23,12 +23,10 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
-          },
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime'],
         },
       },
 
